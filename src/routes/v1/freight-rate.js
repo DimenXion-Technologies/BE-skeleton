2c78a49ed2744bf14779eb0c940d16694bc11d73
Atul Dubey <soaratul@gmail.com>
2025-05-09 15:24:34 +0530

    remove unused files

--- src/routes/v1/freight-rate.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { upload } from '../../utils/rateFileUpload';
-import { uploadFile } from '../../controllers/frController';
-
-const router = express.Router();
-
-router.post('/upload', upload.single('file'), uploadFile);
-
-export default router;
