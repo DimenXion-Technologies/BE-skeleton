e32ac09045c18264bc98b96e074402dc21afafa6
Atul Dubey <atul@dimenxion.in>
2025-05-13 19:29:16 +0530

    remove unused plugins

--- src/utils/common.js
+++
@@ -1,5 +0,0 @@
-import { customAlphabet } from 'nanoid';
-
-// Create nanoid generator with uppercase alphanumeric characters
-export const getUniqueString = (length = 10) =>
-  customAlphabet('ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789', length);
